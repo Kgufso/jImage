36bc02dd1b2ef239eeac645b479adc9c64fd2f6e
Valeriy Chupurnov <chupurnov@gmail.com>
2014-03-27 22:44:03 +0600

    d

--- class.jimage.php
+++ class.jimage.php
@@ -18,10 +18,10 @@ define('USE_AUTO','a');
 	IMG_FILTER_PIXELATE: Applies pixelation effect to the image, use arg1 to set the block size and arg2 to set the pixelation effect mode
 */
 class jImage{
-static public $jpeg_quality = 80;
-static public $image_replace = false;
-static private $mem_types = array('','gif','jpeg','png');
-static private function blank( $width,$height,$use_alpha = false ){
+public $jpeg_quality = 80;
+public $image_replace = false;
+private $mem_types = array('','gif','jpeg','png');
+private function blank( $width,$height,$use_alpha = false ){
 	$image = imagecreatetruecolor($width, $height);
 	if( $use_alpha ){
 		imagesavealpha($image, true);
@@ -30,7 +30,7 @@ static private function blank( $width,$height,$use_alpha = false ){
 	}
 	return $image;
 }
-static public function createFrom( $ext,$file ){
+public function createFrom( $ext,$file ){
 	$func = 'imagecreatefrom'.$ext;
 	if( !is_callable($func) ){ 
 		throw new Exception( 'gb functon '.$func.' no exists' );
@@ -38,27 +38,27 @@ static public function createFrom( $ext,$file ){
 	}
 	return $func($file);
 }
-static public function saveImage( $img,$file_output,$ext='jpeg' ){
+public function saveImage( $img,$file_output,$ext='jpeg' ){
 	if ( $ext == 'jpeg' ) 
-		return imagejpeg($img,$file_output,self::jpeg_quality);
+		return imagejpeg($img,$file_output,$this->jpeg_quality);
 	$func = 'image'.$ext;
 	return $func($img,$file_output);
 }
-static function joinAll( $path,$file_output,$size,$org = USE_HEIGHT,$filter_add = false,$filter_use = false ){
-	if( file_exists( $path ) and ( self::image_replace or !file_exists( $file_output ) ) ){
-		self::size = 0;
-		self::size1 = $size;
-		self::org = $org;
-		self::each($path,function($Photo,$_this){
+function joinAll( $path,$file_output,$size,$org = USE_HEIGHT,$filter_add = false,$filter_use = false ){
+	if( file_exists( $path ) and ($this->image_replace or !file_exists( $file_output )) ){
+		$this->size = 0;
+		$this->size1 = $size;
+		$this->org = $org;
+		$this->each($path,function($Photo,$_this){
 			list($w_i, $h_i, $type) = getimagesize($Photo);
 			$_this->size+= ($_this->org == USE_HEIGHT)?round($w_i*($_this->size1/$h_i)):round($h_i*($_this->size1/$w_i));
 		});
-		$w = (self::org == USE_HEIGHT)?self::size:self::size1;
-		$h = (self::org != USE_HEIGHT)?self::size:self::size1;
-		self::img_o = self::blank( $w, $h );
-		self::x = 0;
-		self::y = 0;
-		self::each($path,function($Photo,$_this){
+		$w = ($this->org == USE_HEIGHT)?$this->size:$this->size1;
+		$h = ($this->org != USE_HEIGHT)?$this->size:$this->size1;
+		$this->img_o = $this->blank( $w, $h );
+		$this->x = 0;
+		$this->y = 0;
+		$this->each($path,function($Photo,$_this){
 			$h = 0;
 			$w = $_this->size1;
 			$src = $_this->resize($Photo,$w,$h,$_this->org);
@@ -68,21 +68,21 @@ static function joinAll( $path,$file_output,$size,$org = USE_HEIGHT,$filter_add 
 			$_this->y+=($_this->org != USE_HEIGHT)?$h:0;
 		});
 		if( $filter_use!==false ){
-			imagefilter(self::img_o, $filter_use);
+			imagefilter($this->img_o, $filter_use);
 		}
 		if( $filter_add!==false ){
-			$img_o1 = self::blank( self::org == USE_HEIGHT?self::size:self::size1*2, self::org != USE_HEIGHT?self::size:self::size1*2 );
-			imagecopy($img_o1, self::img_o, 0, 0, 0, 0, self::org == USE_HEIGHT?self::size:self::size1*2, self::org != USE_HEIGHT?self::size:self::size1*2);
-			imagefilter(self::img_o, $filter_add);//IMG_FILTER_GRAYSCALE);
-			imagecopy($img_o1, self::img_o, self::org == USE_HEIGHT?0:self::size1, self::org != USE_HEIGHT?0:self::size1, 0, 0, self::org == USE_HEIGHT?self::size:self::size1*2, self::org != USE_HEIGHT?self::size:self::size1*2);
-			imagedestroy(self::img_o);
-			return self::saveImage($img_o1,$file_output);
+			$img_o1 = $this->blank( $this->org == USE_HEIGHT?$this->size:$this->size1*2, $this->org != USE_HEIGHT?$this->size:$this->size1*2 );
+			imagecopy($img_o1, $this->img_o, 0, 0, 0, 0, $this->org == USE_HEIGHT?$this->size:$this->size1*2, $this->org != USE_HEIGHT?$this->size:$this->size1*2);
+			imagefilter($this->img_o, $filter_add);//IMG_FILTER_GRAYSCALE);
+			imagecopy($img_o1, $this->img_o, $this->org == USE_HEIGHT?0:$this->size1, $this->org != USE_HEIGHT?0:$this->size1, 0, 0, $this->org == USE_HEIGHT?$this->size:$this->size1*2, $this->org != USE_HEIGHT?$this->size:$this->size1*2);
+			imagedestroy($this->img_o);
+			return $this->saveImage($img_o1,$file_output);
 		}
-		return self::saveImage(self::img_o,$file_output);
+		return $this->saveImage($this->img_o,$file_output);
 	}
 }
-static function resize( $file,&$width,&$height=false,$org=USE_AUTO,&$type='jpeg' ) {
-	if( self::isImage($file,$w_i,$h_i,$type) ){
+function resize( $file,&$width,&$height=false,$org=USE_AUTO,&$type='jpeg' ) {
+	if( $this->isImage($file,$w_i,$h_i,$type) ){
 		if( $org == USE_AUTO ){
 			if($w_i > $h_i )
 				$height = ($width/$w_i) * $h_i;
@@ -97,8 +97,8 @@ static function resize( $file,&$width,&$height=false,$org=USE_AUTO,&$type='jpeg'
 			$width = ( $height/$h_i ) * $w_i;
 		}
 		$ext = $type;
-		$img = self::createFrom($ext,$file);
-		$img_o = self::blank($width, $height);
+		$img = $this->createFrom($ext,$file);
+		$img_o = $this->blank($width, $height);
 		imagecopyresampled($img_o, $img, 0, 0, 0, 0, $width, $height, $w_i, $h_i);
 		imagedestroy($img);
 		return $img_o;
@@ -116,8 +116,8 @@ static function resize( $file,&$width,&$height=false,$org=USE_AUTO,&$type='jpeg'
  * @param array Координаты обрезки, по умолчанию делает квадрат по меньгей из сторон
  * @return bool
  */
-static public function crop($file_input, $file_output, $crop = 'square') {
-	if( !self::isImage( $file_input,$w_i, $h_i, $ext ) )return false;
+function crop($file_input, $file_output, $crop = 'square') {
+	if( !$this->isImage( $file_input,$w_i, $h_i, $ext ) )return false;
 	if ( $crop == 'square' ) {
 		$min = $w_i;
 		if ($w_i > $h_i) $min = $h_i;
@@ -135,13 +135,13 @@ static public function crop($file_input, $file_output, $crop = 'square') {
 	   	if ($h_o < 0) $h_o += $h_i;
 		$h_o -= $y_o;
 	}
-	$img = self::createFrom($ext,$file_input);
-	$img_o = self::blank($w_o, $h_o);
+	$img = $this->createFrom($ext,$file_input);
+	$img_o = $this->blank($w_o, $h_o);
 	imagecopy($img_o, $img, 0, 0, $x_o, $y_o, $w_o, $h_o);
-	self::saveImage( $img_o,$file_output,$ext );
+	$this->saveImage( $img_o,$file_output,$ext );
 	return true;
 }
-static public function clearDir ($directory,&$countfile=0,&$countdir=0){
+function clearDir ($directory,&$countfile=0,&$countdir=0){
 	$dir = opendir($directory);
 	while(($file = readdir($dir))){
 		if ( is_file ($directory."/".$file)){
@@ -155,7 +155,7 @@ static public function clearDir ($directory,&$countfile=0,&$countdir=0){
 	}
 	closedir ($dir);
 }
-static public function each($path,$callback,$ext = 'jpg', $open_dir = false){
+function each($path,$callback,$ext = 'jpg', $open_dir = false){
 	if( file_exists( $path ) and is_dir($path) and is_callable($callback) ){
 		$dir = opendir($path);
 		while(($file = readdir($dir))){
@@ -165,19 +165,19 @@ static public function each($path,$callback,$ext = 'jpg', $open_dir = false){
 				if( in_array($obj,explode(',',$ext)) )
 					$callback( $path."/".$file,$this,$obj,$fi );
 			}else if ( $open_dir and is_dir($path."/".$file) and ($file != ".") and ($file != "..") ){
-				self::each($path."/".$file,$callback,$ext,true); 
+				$this->each($path."/".$file,$callback,$ext,true); 
 			}
 		}
 	}else throw new Exception( 'path '.$path.' no exists' );
 }
-static public function isImage( $filename,&$w=0,&$h=0,&$type='' ){
+public function isImage( $filename,&$w=0,&$h=0,&$type='' ){
 	if(!is_file($filename))return false;
 	list($w, $h, $type1) = getimagesize($filename);
-	$type = @self::mem_types[$type1];
-	return ( !empty($type) and $w and $h and isset(self::mem_types[$type1]) );
+	$type = @$this->mem_types[$type1];
+	return ( !empty($type) and $w and $h and isset($this->mem_types[$type1]) );
 }
-static  public function filter($file,$filter=false, $arg1 = null, $arg2 = null, $arg3 = null, $arg4 = null){
-	if( self::isImage($file,$w,$h,$type) ){
+public function filter($file,$filter=false, $arg1 = null, $arg2 = null, $arg3 = null, $arg4 = null){
+	if($this->isImage($file,$w,$h,$type)){
 		$func = 'imagecreatefrom'.$type;
 		$img = $func($file);
 		if( is_callable($filter) ){
@@ -185,27 +185,26 @@ static  public function filter($file,$filter=false, $arg1 = null, $arg2 = null, 
 		}else if( $filter!==false ){
 			imagefilter($img, $filter,$arg1,$arg2,$arg3,$arg4);
 		}
-		self::saveImage($img,$file.'.'.$type);
-		if(self::image_replace){
+		$this->saveImage($img,$file.'.'.$type);
+		if($this->image_replace){
 			unlink($file);
 			rename($file.'.'.$type,$file);
 		}
 	}
 	
 }
-static  public function thumb( $file,$thumb,$width,$height=false,$org=USE_AUTO ){
-	if(self::image_replace or !file_exists( $thumb ) and self::isImage($file)){
-		$img = self::resize($file,$width,$height,$org,$ext);
-		self::saveImage( $img,$thumb,$ext );
+function thumb( $file,$thumb,$width,$height=false,$org=USE_AUTO ){
+	if($this->image_replace or !file_exists( $thumb ) and $this->isImage($file)){
+		$img = $this->resize($file,$width,$height,$org,$ext);
+		$this->saveImage( $img,$thumb,$ext );
 		imagedestroy($img);
 	}
 	return $thumb;
 }
-
-static  public function _thumb( $file,$width,$height=false,$org=USE_HOWSET ){
+function _thumb( $file,$width,$height=false,$org=USE_HOWSET ){
 	$out = 'files/thumb/'.$width.'x'.$height.'x'.basename(ROOT.$file);
 	if(!file_exists(ROOT.$out))
-		self::thumb(ROOT.$file,ROOT.$out,$width,$height,$org );
+		$this->thumb(ROOT.$file,ROOT.$out,$width,$height,$org );
 	return '/'.$out;
 }
 }
